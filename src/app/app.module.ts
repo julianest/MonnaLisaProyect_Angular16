95af6bd3a75d4e5aefaa3f5f2b326fe73f1f861c
julian.huerfano <julianest12@gmail.com>
2025-01-24 20:01:00 -0500

    Login, register (Html, Css) [Add]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './modules/auth/views/login/login.component';
+import { RegisterComponent } from './modules/auth/views/register/register.component';
+import { HomeComponent } from './modules/general/views/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
