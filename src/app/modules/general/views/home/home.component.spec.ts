9c5f9e5061150f4393d1ff041c1320831f4eec9a
julian.huerfano <julianest12@gmail.com>
2025-02-05 23:25:59 -0500

    [ADD] Se agregan pruebas a varios componentes

--- src/app/modules/general/views/home/home.component.spec.ts
+++ src/app/modules/general/views/home/home.component.spec.ts
@@ -1,23 +1,68 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HomeComponent } from './home.component';
+import { ReactiveFormsModule, FormBuilder } from '@angular/forms';
+import { GeneralService } from '../general.service';
+import { Alertas } from 'utils/alerts';
+import { of, throwError } from 'rxjs';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let generalServiceMock: jasmine.SpyObj<GeneralService>;
+  let alertMock: jasmine.SpyObj<Alertas>;
 
   beforeEach(() => {
+    const generalServiceSpy = jasmine.createSpyObj('GeneralService', ['getInfoUser', 'filtrerTransaction', 'findBalance']);
+    const alertSpy = jasmine.createSpyObj('Alertas', ['cerrar', 'warning', 'loading', 'error']);
+
     TestBed.configureTestingModule({
+      imports: [ReactiveFormsModule, HttpClientTestingModule],
       declarations: [HomeComponent],
-      imports: [HttpClientTestingModule]
-    });
+      providers: [
+        FormBuilder,
+        { provide: GeneralService, useValue: generalServiceSpy },
+        { provide: Alertas, useValue: alertSpy },
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    generalServiceMock = TestBed.inject(GeneralService) as jasmine.SpyObj<GeneralService>;
+    alertMock = TestBed.inject(Alertas) as jasmine.SpyObj<Alertas>;
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+
+  it('debería ejecutar homeFilter y manejar la respuesta correctamente', () => {
+    const account = 123;
+    component.homeFilterForm.controls['inputAccount'].setValue(account);
+
+    generalServiceMock.filtrerTransaction.and.returnValue(of({
+      code: 200,
+      response: [{ tipoMovimiento: 'Depósito', fechaMovimiento: '2025-02-05', monto: 100 }]
+    }));
+    generalServiceMock.findBalance.and.returnValue(of({ response: { saldo: 1000 } }));
+
+    component.homeFilter();
+
+    expect(alertMock.loading).toHaveBeenCalled();
+    expect(generalServiceMock.filtrerTransaction).toHaveBeenCalledWith(account);
+    expect(generalServiceMock.findBalance).toHaveBeenCalledWith(account);
+    expect(component.transactionResponse.length).toBeGreaterThan(0);
+    expect(component.saldoActual).toBe('1000');
+    expect(alertMock.cerrar).toHaveBeenCalled();
+  });
+
+  it('debería manejar un error en homeFilter correctamente', () => {
+    const account = 123;
+    component.homeFilterForm.controls['inputAccount'].setValue(account);
+
+    generalServiceMock.filtrerTransaction.and.returnValue(throwError(() => new Error('Error de red')));
+
+    component.homeFilter();
+
+    expect(alertMock.error).toHaveBeenCalledWith('Error desconocido', 'Por favor inténtelo más tarde');
   });
 });
--- src/app/modules/general/views/withdraw/withdraw.component.spec.ts
+++ src/app/modules/general/views/withdraw/withdraw.component.spec.ts
@@ -1,23 +1,65 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { WithdrawComponent } from './withdraw.component';
+import { ReactiveFormsModule, FormBuilder } from '@angular/forms';
+import { GeneralService } from '../general.service';
+import { Alertas } from 'utils/alerts';
+import { StreamNotificationService } from 'src/app/services/stream-notification.service';
+import { throwError } from 'rxjs';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('WithdrawComponent', () => {
   let component: WithdrawComponent;
   let fixture: ComponentFixture<WithdrawComponent>;
+  let generalServiceMock: jasmine.SpyObj<GeneralService>;
+  let alertMock: jasmine.SpyObj<Alertas>;
 
   beforeEach(() => {
+    const generalServiceSpy = jasmine.createSpyObj('GeneralService', ['getInfoUser', 'withdrawTransaction']);
+    const alertSpy = jasmine.createSpyObj('Alertas', ['cerrar', 'warning', 'loading', 'error']);
+    const streamNotificationSpy = jasmine.createSpyObj('StreamNotificationService', ['getStreamTransactionNotifications', 'setTransactionType']);
+
     TestBed.configureTestingModule({
+      imports: [ReactiveFormsModule, HttpClientTestingModule],
       declarations: [WithdrawComponent],
-      imports: [HttpClientTestingModule]
-    });
+      providers: [
+        FormBuilder,
+        { provide: GeneralService, useValue: generalServiceSpy },
+        { provide: Alertas, useValue: alertSpy },
+        { provide: StreamNotificationService, useValue: streamNotificationSpy },
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(WithdrawComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    generalServiceMock = TestBed.inject(GeneralService) as jasmine.SpyObj<GeneralService>;
+    alertMock = TestBed.inject(Alertas) as jasmine.SpyObj<Alertas>;
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+it('debería manejar un error en withdrawMoney correctamente', () => {
+    const account = 123;
+    const amount = 100;
+    component.withdrawForm.controls['inputAccount'].setValue(account);
+    component.withdrawForm.controls['inputWithdraw'].setValue(amount);
+
+    generalServiceMock.withdrawTransaction.and.returnValue(throwError(() => new Error('Error al realizar el retiro')));
+
+    component.withdrawMoney();
+
+    expect(alertMock.error).toHaveBeenCalledWith('Error desconocido', 'Por favor inténtelo más tarde');
+  });
+
+  it('debería manejar un error en withdrawMoney correctamente', () => {
+    const account = 123;
+    const amount = 100;
+    component.withdrawForm.controls['inputAccount'].setValue(account);
+    component.withdrawForm.controls['inputWithdraw'].setValue(amount);
+
+    generalServiceMock.withdrawTransaction.and.returnValue(throwError(() => new Error('Error al realizar el retiro')));
+
+    component.withdrawMoney();
+
+    expect(alertMock.error).toHaveBeenCalledWith('Error desconocido', 'Por favor inténtelo más tarde');
   });
 });
